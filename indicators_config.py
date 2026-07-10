1d7527aef93f569ae6882bfbf1793cd0eae97a6b
yigitarda <yigit9188@gmail.com>
2026-07-10 09:08:45 +0300

    Update indicators_config.py

--- indicators_config.py
+++ indicators_config.py
@@ -28,11 +28,12 @@ servoGauge_1    = IndicatorWidget("gauge", -10, +10)
 servoGauge_2    = IndicatorWidget("gauge", -15, +15)
 rollGauge       = IndicatorWidget("gauge", -15, +15)
 speedometer_ind = IndicatorWidget("speedometer", 0, 50)
-thermometer_ind = IndicatorWidget("thermometer", -10, 40)
+thermometer_ind = IndicatorWidget("thermometer", -30, 60)
 altitude_ind    = IndicatorWidget("altitude", 0, 3000)
 artificial_horizon = IndicatorWidget("artificial_horizon", 0, 30)
-lidar_alt_ind = IndicatorWidget("altitude",0,100)
-pitot_speed_ind = IndicatorWidget("speedometer",0,250)
+lidar_alt_ind = IndicatorWidget("altitude",0,200)
+pitot_speed_ind = IndicatorWidget("speedometer",0,30)
+
 
 # Configuration mapping: [parser_function_name, output_key, widget_blueprint, ui_label]
 indicatorsList = [
@@ -57,7 +58,7 @@ indicatorsList = [
     ["servoAndTemp", "ambientTemp",    thermometer_ind,  "Ambient Temp"],
     ["attitudeValues", "attitude",     artificial_horizon, "Artificial Horizon(pitch,roll)"],
     ["lidar_function","lidar_distance", lidar_alt_ind, "Lidar Alt(m)"],
-    ["pitot_function","pitot_speed",pitot_speed_ind,  "Airspeed(km/h)"],
+    ["pitot_function","pitot_speed",pitot_speed_ind,  "Airspeed(m/s)"],
     ["pitot_function","temperature", thermometer_ind, "Pitot Temp(°C)"]
 ]
 
